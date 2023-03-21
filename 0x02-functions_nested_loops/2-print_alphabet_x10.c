1c14b6bfc114ab6df538f143f0d3b17aba77d404
Divine-95 <divinejustice95@gmail.com>
2023-03-21 08:09:38 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putcar(ch);
+			ch++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
